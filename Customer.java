5f3e377ff72ca6597aaa29d0116fd61c67d654da
Sonbul <sonbul.karim@gmail.com>
2021-11-19 17:26:11 +0200

    Added Customer class but class diagram needs to be adjusted as there must be an aggregation relation between offer class and Customer class

---
+++ Customer.java
@@ -0,0 +1,29 @@
+public class Customer extends User{
+    private Offer[] offers;
+    private RideRequest rideRequest;
+
+    Customer(){
+
+    }
+    Customer(String id, String email, String pwd, int mobile){
+
+    }
+    public void register(){
+
+    }
+    public void request(String src, String dst){
+
+    }
+    public void recieveOffer(Offer o){
+
+    }
+    public void listOffers(){
+
+    }
+    public void selectOffer(Offer o){
+
+    }
+    public void rateCompletedRide(){
+        
+    }
+}
