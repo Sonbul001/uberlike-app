d5ea7e0d15deaeb703391c50408369c51dcc928d
Sonbul <sonbul.karim@gmail.com>
2021-11-19 17:20:37 +0200

    Added Admin class

---
+++ Admin.java
@@ -0,0 +1,20 @@
+public class Admin extends User{
+    private Driver[] pendingDrivers;
+    private int PDcount;
+
+    public void setPendingDriver(Driver d){
+
+    }
+    public void listPendingDrivers(){
+
+    }
+    public void approveDriver(Driver d){
+
+    }  
+    public void denyDriver(Driver d){
+
+    }
+    public void register(){
+        
+    }
+}
