e319a954fe62bc5e72bb7d686302ff47f83748d3
Sonbul <sonbul.karim@gmail.com>
2021-11-19 17:14:39 +0200

    Added Registeration System and made changes to DriverRegisteration so that it can correctly implement the Registeration interface

---
+++ CustomerRegisteration.java
@@ -0,0 +1,5 @@
+public class CustomerRegisteration {
+    public void register(){
+        
+    }
+}
---
+++ DriverRegisteration.java
@@ -0,0 +1,11 @@
+public class DriverRegisteration implements Registeration{
+    private Admin admin;
+    private Driver driver;
+    
+    public void addAdmin(Admin a){
+
+    }
+    public void register(){
+        
+    }
+}
