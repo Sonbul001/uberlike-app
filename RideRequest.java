b339df3228c930fef30af6d706a6e3b9d924c0ad
Sonbul <sonbul.karim@gmail.com>
2021-11-20 21:04:18 +0200

    changed drivers array into arrayList and added three new getter functions and implemented the whole class

--- RideRequest.java
+++ RideRequest.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
+
 public class RideRequest {
-    private Driver[] drivers;
+    private ArrayList<Driver> drivers = new ArrayList<Driver>();
+    private Customer customer;
+    private String source;
+    private String destination;
 
+    RideRequest(Customer c, String src, String dst){
+        customer = c;
+        source = src;
+        destination = dst;
+    }
     public void addDriver(Driver d){
-
+        drivers.add(d);
     }
     public void removeDriver(Driver d){
-
+        drivers.remove(d);
     }
     public void publishRequest(Customer c, String src, String dst){
-        
+        for (Driver driver : drivers) {
+            if(driver.getFavAreas().contains(src))
+                driver.recieveRequest(c, src, dst);
+        }
+    }
+    public Customer getCustomer(){
+        return customer;
+    }
+    public String getSource(){
+        return source;
+    }
+    public String getDestination(){
+        return destination;
     }
 }
